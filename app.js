92a4e3f43c084a4d64428b2a2049046eaf1b84f4
Vikram Rana <vikram.rana@vikramrana-ltm.internal.salesforce.com>
2018-02-07 23:29:03 -0800

    missing )

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ db.sequelize.sync().then(function() {
     res.sendFile('index.html');
     //It will find and locate index.html from public
   });
-};
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
