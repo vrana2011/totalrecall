e40fe3f6d392ce92ba5b854a0431e2481084b64e
Vikram Rana <vikram.rana@vikramrana-ltm.internal.salesforce.com>
2018-02-09 11:40:14 -0800

    highlight for search results

--- routes/recordings.js
+++ routes/recordings.js
@@ -42,7 +42,7 @@ router.get('/', function(req, res, next) {
     
       for (var h in content.hits) {
         console.log(
-          `Hit(${content.hits[h].objectID}): ${content.hits[h]._highlightResult[0].transcript.value}`
+          `Hit(${content.hits[h].objectID}): ${content.hits[h]._highlightResult.transcript.value}`
         );
         var titleResult = content.hits[h].title;
         var transcriptResult = content.hits[h]._highlightResult.value;
