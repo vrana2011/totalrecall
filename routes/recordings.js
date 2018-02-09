2a6c66dc14f44ea8135e48d661b52e8eed04135e
Vikram Rana <vikram.rana@vikramrana-ltm.internal.salesforce.com>
2018-02-09 00:32:19 -0800

    fix bug

--- routes/recordings.js
+++ routes/recordings.js
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
   winston.log('info', 'query string: ' + q);
   
   // search
-  var recordingsList = [];
+  var recordings = [];
   if(q === null || typeof q === "undefined") {
     winston.log('info', 'query string null or undefined: ' + q);
   } else {
@@ -43,7 +43,7 @@ router.get('/', function(req, res, next) {
         var titleResult = content.hits[h].title;
         console.log('title: ' + titleResult);
         var recording = [{title: titleResult}]; 
-        recordingsList.push(recording);
+        recordings.push(recording);
       }
     });
     
